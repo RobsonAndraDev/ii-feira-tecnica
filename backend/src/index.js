588204930fe2e0e1a9639ce33f305dca3d00771f
RobsonAndradev <robsom.s.a@gmail.com>
2021-10-30 14:05:51 -0300

    Use cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const app = require("express")()
 const bodyParser = require("body-parser")
+const cors = require('cors')
 const fs = require('fs')
 const { validateToken } = require("./middlewares/authentication")
 const useCaseFolder = `${__dirname}/use-cases`
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use(validateToken);
 
 fs.readdirSync(useCaseFolder).forEach(folder => {
@@ -13,6 +15,6 @@ fs.readdirSync(useCaseFolder).forEach(folder => {
   controler.set(app)
 })
 
-app.listen(3000, () => {
-  console.log("magic happens on 3000")
+app.listen(5000, () => {
+  console.log("magic happens on 5000")
 });
--- backend/src/use-cases/login/index.js
+++ backend/src/use-cases/login/index.js
@@ -2,7 +2,6 @@ const service = require('./service')
 
 function set(app) {
   app.post("/login", login);
-  app.get("/users", getUsers);
 }
 
 function login(req, res, next) {
@@ -11,10 +10,6 @@ function login(req, res, next) {
   const token = service.login(username, password)
 
   return res.status(token.code).json(token)
-}
-
-function getUsers(req, res) {
-  return res.json(service.getUsers());
 }
 
 module.exports = {
